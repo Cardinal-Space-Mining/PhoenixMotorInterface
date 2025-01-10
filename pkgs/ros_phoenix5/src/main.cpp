67481e9008ee867e3c67bda54d9d60b642d54d59
db-dual <dylanbooth06@gmail.com>
2025-01-10 15:51:27 -0600

    push before change branch

--- pkgs/ros_phoenix5/src/main.cpp
+++ pkgs/ros_phoenix5/src/main.cpp
@@ -94,20 +94,20 @@ private:
 
     custom_types::msg::TalonInfo get_info(TalonSRX &motor) {
         custom_types::msg::TalonInfo info;
-        // info.temperature    = motor.GetTemperature();
-        // info.bus_voltage    = motor.GetBusVoltage();
-        // info.output_percent = motor.GetMotorOutputPercent();
-        // info.output_voltage = motor.GetMotorOutputVoltage();
-        // info.output_current = motor.GetOutputCurrent();
-        // info.position       = motor.GetSelectedSensorPosition();
-        // info.velocity       = motor.GetSelectedSensorVelocity();
-        info.temperature = 0;
-        info.bus_voltage = 0;
-        info.output_percent = 0;
-        info.output_voltage = 0;
-        info.output_current = 0;
-        info.position = 0;
-        info.velocity = 0;
+        info.temperature    = motor.GetTemperature();
+        info.bus_voltage    = motor.GetBusVoltage();
+        info.output_percent = motor.GetMotorOutputPercent();
+        info.output_voltage = motor.GetMotorOutputVoltage();
+        info.output_current = motor.GetOutputCurrent();
+        info.position       = motor.GetSelectedSensorPosition();
+        info.velocity       = motor.GetSelectedSensorVelocity();
+        // info.temperature = 0;
+        // info.bus_voltage = 0;
+        // info.output_percent = 0;
+        // info.output_voltage = 0;
+        // info.output_current = 0;
+        // info.position = 0;
+        // info.velocity = 0;
 
         return info;
     }
